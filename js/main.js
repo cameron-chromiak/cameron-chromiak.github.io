573d50f2702a4ad31714ae4c8952c17b1056a2e6
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-01 10:45:05 -0500

    added changing text, updated projects area, border disapears around menu toggle after first click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-//navbar
-$(document).ready(function(){
-    $('.navbar').hide()
 
+$(document).ready(function(){
+  //navbar
+    $('.navbar').hide()
     $(window).scroll(function(){
         if($(this).scrollTop() < 50){
           $('.navbar').fadeOut()
@@ -9,4 +9,19 @@ $(document).ready(function(){
           $('.navbar').fadeIn()
         }
     })
+
+    // greeting
+    //greeting
+    const greetings = ['Hello', 'Hola', 'Namaskar', 'God dag', 'Bonjour', 'Shalom',
+    'Hallo', 'Zdravstvuyte', '你好']
+
+    setInterval(function () {
+      $('#welcome').animate({opacity:0})
+      .queue(function(){
+        $(this).text(greetings[Math.floor(Math.random()*greetings.length)])
+        .dequeue()
+      }).animate({opacity: 1})
+      // $('#welcome').text(greetings[Math.floor(Math.random()*greetings.length)]).fadeIn()
+}, 2500); // Execute somethingElse() every 2 seconds.
+
 })
